298e029b6bc64620d5565d508d5ec39ec7851720
Maryam Abdel-karim <maryam.abdelkarim@hotmail.com>
2020-12-15 19:07:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ function Scooter(year, color, model){
   
 }
 
+<<<<<<< HEAD
 function Driver(name, age, experience){
+=======
+function Scooter(name, age, experience){
+>>>>>>> 252a386d07613bf57a2bc9348b6c7edd5e399867
 
     this.name =  name;
     this.age = age;
